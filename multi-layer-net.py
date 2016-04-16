2f10d8b29a64aa1c135472a09f5d12a5511c9e65
Sergei Iakhnin <llevar@gmail.com>
2016-04-16 17:31:13 +0200

    Add plots

--- multi-layer-net.py
+++ multi-layer-net.py
@@ -2,6 +2,9 @@ from __future__ import print_function
 import numpy as np
 import tensorflow as tf
 from six.moves import cPickle as pickle
+import matplotlib.pyplot as plt
+from scipy.interpolate import spline
+import math
 
 pickle_file = '/Users/siakhnin/Documents/workspace/udacity_deep_learning/notMNIST.pickle'
 
@@ -14,9 +17,9 @@ with open(pickle_file, 'rb') as f:
   test_dataset = save['test_dataset']
   test_labels = save['test_labels']
   del save  # hint to help gc free up memory
-  print('Training set', train_dataset.shape, train_labels.shape)
-  print('Validation set', valid_dataset.shape, valid_labels.shape)
-  print('Test set', test_dataset.shape, test_labels.shape)
+#  print('Training set', train_dataset.shape, train_labels.shape)
+#  print('Validation set', valid_dataset.shape, valid_labels.shape)
+#  print('Test set', test_dataset.shape, test_labels.shape)
   
 image_size = 28
 num_labels = 10
@@ -29,11 +32,11 @@ def reformat(dataset, labels):
 train_dataset, train_labels = reformat(train_dataset, train_labels)
 valid_dataset, valid_labels = reformat(valid_dataset, valid_labels)
 test_dataset, test_labels = reformat(test_dataset, test_labels)
-print('Training set', train_dataset.shape, train_labels.shape)
-print('Validation set', valid_dataset.shape, valid_labels.shape)
-print('Test set', test_dataset.shape, test_labels.shape)
+#print('Training set', train_dataset.shape, train_labels.shape)
+#print('Validation set', valid_dataset.shape, valid_labels.shape)
+#print('Test set', test_dataset.shape, test_labels.shape)
 
-max_steps = 10000
+max_steps = 1000
 learning_rate = 0.001
 dropout = 0.9
 data_dir = '/tmp/data'
@@ -62,7 +65,7 @@ def variable_summaries(var, name):
         tf.scalar_summary('min/' + name, tf.reduce_min(var))
         tf.histogram_summary(name, var)
 
-def nn_layer(input_tensor, input_dim, output_dim, layer_name):
+def nn_layer(input_tensor, input_dim, output_dim, layer_name, act=tf.nn.relu):
     """Reusable code for making a simple neural net layer.
 
     It does a matrix multiply, bias add, and then uses relu to nonlinearize.
@@ -71,19 +74,20 @@ def nn_layer(input_tensor, input_dim, output_dim, layer_name):
     """
     # Adding a name scope ensures logical grouping of the layers in the graph.
     with tf.name_scope(layer_name):
-        # This Variable will hold the state of the weights for the layer
-        with tf.name_scope("weights"):
-            weights = weight_variable([input_dim, output_dim])
-            variable_summaries(weights, layer_name + '/weights')
-        with tf.name_scope("biases"):
-            biases = bias_variable([output_dim])
-            variable_summaries(biases, layer_name + '/biases')
-        with tf.name_scope('Wx_plus_b'):
-            activations = tf.matmul(input_tensor, weights) + biases
-            tf.histogram_summary(layer_name + '/activations', activations)
-        relu = tf.nn.relu(activations, 'relu')
-        tf.histogram_summary(layer_name + '/activations_relu', relu)
-    return tf.nn.dropout(relu, keep_prob), weights
+      # This Variable will hold the state of the weights for the layer
+      with tf.name_scope('weights'):
+        weights = weight_variable([input_dim, output_dim])
+        variable_summaries(weights, layer_name + '/weights')
+      with tf.name_scope('biases'):
+        biases = bias_variable([output_dim])
+        variable_summaries(biases, layer_name + '/biases')
+      with tf.name_scope('Wx_plus_b'):
+        preactivate = tf.matmul(input_tensor, weights) + biases
+        tf.histogram_summary(layer_name + '/pre_activations', preactivate)
+      activations = act(preactivate, 'activation')
+      tf.histogram_summary(layer_name + '/activations', activations)
+      return tf.nn.dropout(activations, keep_prob), weights
+
 
 graph = tf.Graph()
 with graph.as_default():
@@ -95,11 +99,19 @@ with graph.as_default():
         y_ = tf.placeholder(dtype=tf.float32, shape=[None, 10], name="y-input")
         keep_prob = tf.placeholder(dtype=tf.float32, name="dropout-keep-prob")
         tf.scalar_summary('dropout_keep_probability', keep_prob)
-        reg_rate = tf.placeholder(tf.float32, name="regularization-rate")    
+        reg_rate = tf.placeholder(tf.float32, name="regularization-rate")
+        
 
+    
+    
     layer1, weights_1 = nn_layer(x, num_features, 1024, 'layer1')
     layer2, weights_2 = nn_layer(layer1, 1024, num_labels, 'layer2')
     y = tf.nn.softmax(layer2, 'predictions')
+    #layer1, weights_1 = nn_layer(x, num_features, 1024, 'layer1')
+    #layer2, weights_2 = nn_layer(layer1, 1024, 300, 'layer2')
+    #layer3, weights_3 = nn_layer(layer2, 300, 50, 'layer3')
+    #layer4, weights_4 = nn_layer(layer3, 50, num_labels, 'layer4')
+    #y = tf.nn.softmax(layer4, 'predictions')
 
 
     with tf.name_scope('cross_entropy'):
@@ -110,11 +122,16 @@ with graph.as_default():
             normalized_cross_entropy = -tf.reduce_mean(diff)
         tf.scalar_summary('cross entropy', normalized_cross_entropy)
         
-    regularizer = reg_rate * (tf.nn.l2_loss(weights_1) + tf.nn.l2_loss(weights_2))
+    #regularizer = reg_rate * (tf.nn.l2_loss(weights_1) + tf.nn.l2_loss(weights_1))
+    regularizer = 0
+    #regularizer = reg_rate * (tf.nn.l2_loss(weights_1) + tf.nn.l2_loss(weights_2) + tf.nn.l2_loss(weights_3) + tf.nn.l2_loss(weights_4))
     loss_func = cross_entropy + regularizer
     
     with tf.name_scope('train'):
-        train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss_func)
+        current_step = tf.placeholder(tf.float32, name="current-step")
+        #learning_rate = tf.train.exponential_decay(0.001, current_step, max_steps, 0.96)
+        #learning_rate = learning_rate * math.pow(0.96, float(current_step)/1000)
+        train_step = tf.train.GradientDescentOptimizer(0.001).minimize(loss_func)
 
     with tf.name_scope('accuracy'):
         with tf.name_scope('correct_prediction'):
@@ -132,6 +149,11 @@ with  tf.Session(graph=graph) as my_session:
 
 
     tf.initialize_all_variables().run()
+    
+    train_accuracy = []
+    train_epochs = []
+    validation_accuracy = []
+    validation_epochs = []
 
 
     for step in range(max_steps):
@@ -147,20 +169,48 @@ with  tf.Session(graph=graph) as my_session:
         if step % 500 == 0:
             feed_dict = {x: valid_dataset, y_: valid_labels, keep_prob: 1, reg_rate: 0.001}
             summary, my_accuracy = my_session.run([merged, accuracy], feed_dict=feed_dict)
+            validation_accuracy.append(my_accuracy)
+            validation_epochs.append(step)
             test_writer.add_summary(summary, step)
             print('Validation accuracy at step %s: %s' % (step, my_accuracy))
+            
         else:
-            feed_dict = {x: batch_data, y_: batch_labels, keep_prob: dropout, reg_rate: 0.001}
+            feed_dict = {x: batch_data, y_: batch_labels, keep_prob: dropout, reg_rate: 0.001, current_step: step}
             summary, my_accuracy, _ = my_session.run([merged, accuracy, train_step], feed_dict=feed_dict)
             train_writer.add_summary(summary, step)
-            
-
-        #if (step % 500 == 0):
-        #  print("Minibatch loss at step %d: %f" % (step, l))
-        #  print("Minibatch accuracy: %.1f%%" % accuracy(predictions, batch_labels))
-        #  print("Validation accuracy: %.1f%%" % accuracy(
-        #    valid_prediction.eval(), valid_labels))
+            train_accuracy.append(my_accuracy)
+            train_epochs.append(step)
+    
+    fig,ax = plt.subplots(1,1)
+    train_time_subsample = np.array(train_epochs[::200])
+    train_data_subsample = np.array(train_accuracy[::200])
+    x_smooth = np.linspace(train_time_subsample[0], train_time_subsample[-1], 300)
+    y_smooth = spline(train_time_subsample,train_data_subsample,x_smooth)
+    
+    ax.plot(x_smooth,y_smooth, 'g-', label="Training Accuracy")
+    ax.plot(validation_epochs,validation_accuracy, 'ro')
+    ax.plot(validation_epochs,validation_accuracy, 'b-', label="Validation Accuracy")
+    
+    ax.set_xlabel("Epoch")
+    ax.set_ylabel("Accuracy")
+    ax.set_xlim(0, max_steps * 1.05)
+    ax.set_ylim(0, 1.1)
+   
+    
+    plt.draw()
+       
     feed_dict = {x: test_dataset, y_: test_labels, keep_prob: 1, reg_rate: 0.001}
     summary, my_accuracy = my_session.run([merged, accuracy], feed_dict=feed_dict)
+    
+    ax.plot(max_steps, my_accuracy, 'go', markersize=5, label="Test Accuracy")
+    plt.annotate("Test Accuracy", xy = (max_steps, my_accuracy), xytext=(-20,20), 
+                 textcoords='offset points', ha='right', va='bottom',
+                 bbox = dict(boxstyle = 'round,pad=0.5', fc = 'yellow', alpha = 0.5),
+                 arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3,rad=0'))
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles, labels, loc=4)
+    plt.draw()
+    
     test_writer.add_summary(summary)
-    print("Test accuracy: %.1f%%" % my_accuracy)
+    print("Test accuracy: %s" % my_accuracy)
+    plt.show()
